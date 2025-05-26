49fa8a20fa0908d29e9845885fdfdd4f88ac042e
MikkoAsahi <kamikko0317@gmail.com>
2025-05-26 09:11:23 +0800

    ....

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'admin/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_headers' => ['*'],
+    'supports_credentials' => true,
+];
