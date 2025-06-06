90af296b13385504433386e5ac3e7b45b1848655
MikkoAsahi <kamikko0317@gmail.com>
2025-06-06 13:30:51 +0800

    ...

--- app/Filament/Resources/BookingResource/Pages/ListBookings.php
+++ app/Filament/Resources/BookingResource/Pages/ListBookings.php
@@ -87,7 +87,7 @@ class ListBookings extends ListRecords
                                                 'quantity' => 1,
                                             ]
                                         ],
-                                        'payment_method_types' => ['gcash', 'card', 'paymaya'],
+                                        'payment_method_types' => ['gcash', 'card', 'paymaya','brankas_bdo','brankas_landbank','brankas_metrobank'],
                                         'success_url' => route('payment.success'),
                                         'cancel_url' => route('payment.failure'),
                                         'metadata' => [
